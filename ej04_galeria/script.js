d94beeccc917462e4e44522fce4eb8db6f587671
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-09-22 11:06:54 +0200

    ej4 (galería) terminado

--- ej04_galeria/script.js
+++ ej04_galeria/script.js
@@ -11,15 +11,32 @@ let fotos = [
 let foto_actual = 1
 
 let botones = document.querySelectorAll("button")
-botones[0].addEventListener("click", function(){
-    if (foto_actual > 0)
-        foto_actual--
 
-    img.src = "img/" + fotos[foto_actual]
+botones[0].addEventListener("click", function(){
+    cambiarImagen("izquierda")
 })
+
 botones[1].addEventListener("click", function(){
-    if (foto_actual < fotos.length-1)
-        foto_actual++
-    
-    img.src = "img/" + fotos[foto_actual]
+    cambiarImagen("derecha")
 })
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+      cambiarImagen('izquierda');
+    } else if (event.key === 'ArrowRight') {
+      cambiarImagen('derecha');
+    }
+});
+
+function cambiarImagen(direccion) {
+    if (direccion == "izquierda") {
+        foto_actual--
+        if (foto_actual < 0)
+            foto_actual = fotos.length-1
+        img.src = "img/" + fotos[foto_actual]
+    } else if (direccion == "derecha") {
+        foto_actual++
+        foto_actual = foto_actual % fotos.length
+        img.src = "img/" + fotos[foto_actual]
+    }
+}
