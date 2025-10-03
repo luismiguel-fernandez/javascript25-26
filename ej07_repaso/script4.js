de07adfe282b388a0ebad310f60b476b2da8981f
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-10-03 12:35:57 +0200

    Ej atrapa la bola planteado

--- ej07_repaso/script4.js
+++ ej07_repaso/script4.js
@@ -16,11 +16,12 @@ let plantarseBtn = document.querySelector("#plantarseBtn")
 let marcadorDiv = document.querySelector("#marcadorDiv")
 
 let cartasJugador = []
+let totalJugador = 0
 
 cartaBtn.addEventListener("click",function(){
   let nuevaCarta = Math.floor(Math.random()*11) + 1 //genero carta entre 1 y 11
   cartasJugador.push(nuevaCarta)
-  let totalJugador = sumArray(cartasJugador)
+  totalJugador = sumArray(cartasJugador)
   marcadorDiv.textContent = cartasJugador + " = " + totalJugador
   cartaBtn.textContent = "Sacar otra carta más"
   if (totalJugador > 21) {
@@ -33,7 +34,37 @@ cartaBtn.addEventListener("click",function(){
   }
 })
 
+plantarseBtn.addEventListener("click",function(){
+  //si el jugador no ha sacado ninguna carta todavía, este clic no hace nada (return)
+  if (cartasJugador.length == 0) return
+
+  //juega el crupier
+  let totalCrupier = 0
+  while (totalCrupier < 17 /*&& totalCrupier < totalJugador*/ ) {
+    let cartaCrupier = Math.floor(Math.random()*11) + 1
+    marcadorDiv.innerHTML += "<br> El crupier saca la carta " + cartaCrupier
+    totalCrupier = totalCrupier + cartaCrupier
+  }
+  marcadorDiv.innerHTML += "<br>El crupier ha obtenido " + totalCrupier
+  //Una de dos: o el crupier ha pasado o igualado 17, o ha superado al jugador
+  //Comprobar primero si se ha pasado
+  if (totalCrupier > 21 || totalCrupier < totalJugador) {
+    //el jugador gana
+    marcadorDiv.innerHTML += "<br>Ganas la partida"
+  } else if (totalCrupier == totalJugador) {
+    //el jugador empata con el crupier
+    marcadorDiv.innerHTML += "<br>Empatas la partida"
+  } else {
+    //el jugador pierde
+    marcadorDiv.innerHTML += "<br>Pierdes la partida"
+  }
+  cartasJugador = []
+  cartaBtn.textContent = "Empezar"
+})
 
 function sumArray(arr) {
   return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
+
+//botonEmpezar.style.display = "none"
+//botonEmpezar.disabled = true
