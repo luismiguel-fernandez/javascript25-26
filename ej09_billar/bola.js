540b62ceb2967a037f84c5fd8ed5c5acd8dfa62f
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-10-06 11:28:05 +0200

    ej9 billar planteado

---
+++ ej09_billar/bola.js
@@ -0,0 +1,10 @@
+class Bola {
+    constructor(punteroDiv, top0, left0, vely0, velx0, color0) {
+        this.punteroDiv = punteroDiv
+        this.top = top0
+        this.left = left0
+        this.vely = vely0
+        this.velx = velx0
+        this.color = color0
+    }
+}
---
+++ ej09_billar/script.js
@@ -0,0 +1,11 @@
+//recuperar elementos del DOM
+const tablero = document.querySelector("#tablero")
+
+const ANCHURA_TABLERO = 900
+const ALTURA_TABLERO = 450
+const DIAMETRO_BOLA = 30
+
+//código automático
+
+
+//funciones auxiliares
