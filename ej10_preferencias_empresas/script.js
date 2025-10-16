9f1c469cacaebf6ced463994a715290f5d0de750
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-10-16 12:26:31 +0200

    ej10 casi terminado

--- ej10_preferencias_empresas/script.js
+++ ej10_preferencias_empresas/script.js
@@ -10,8 +10,14 @@ const EMPRESAS = [
     "Patatas Acho"
 ]
 
+let choices = []
+
+const studentName = document.querySelector("#studentName")
 const choice1 = document.querySelector("#choice1")
 const choice2 = document.querySelector("#choice2")
+const insertButton = document.querySelector("#insertButton")
+const errorMsg = document.querySelector("#errorMsg")
+const cuerpoTabla = document.querySelector("#studentsChoices>tbody")
 
 //RETO #1: rellenar el primer SELECT con estas empresas
 EMPRESAS.forEach( (emp,index) => {
@@ -29,23 +35,69 @@ EMPRESAS.forEach( (emp,index) => {
 }*/
 
 
-
-
-
 //RETO #2: que al elegir una empresa en el primer SELECT se rellene
 // el segundo SELECT con el resto de empresas no elegidas
 choice1.addEventListener("change",function(){
+    choice2.innerHTML = '<option value="0">(choose one)</option>'
     if (choice1.value > 0) {
         //bucle para rellenar choice2
-        choice2.innerHTML = '<option value="0">(choose one)</option>'
         EMPRESAS.forEach( (emp,index) => {
-
             if (choice1.value != index + 1) {
                 let nuevoOption = document.createElement("OPTION")
                 nuevoOption.textContent = emp
                 choice2.append(nuevoOption)
             }
-
         })
     }
 })
+
+//RETO #3: si el formulario está relleno, al hacer clic en el botón
+// se inserta una nueva fila en la tabla
+insertButton.addEventListener("click", function(){
+    if (studentName.value != 0 && choice1.value && choice2.value) {
+        choices.push({
+            name: studentName.value,
+            choice1: choice1.selectedOptions[0].textContent,
+            choice2: choice2.selectedOptions[0].textContent
+        })
+        addRow()
+
+        errorMsg.textContent = ""
+        //resetear formulario
+        studentName.value = ""
+        choice1.value = 0
+        choice2.innerHTML = '<option value="0">(choose one)</option>'
+        studentName.focus()
+    } else {
+        //sacar un mensaje de error en el LABEL errorMsg
+        errorMsg.textContent = "El formulario no está bien rellenado"
+    }
+})
+
+function addRow(){
+    //añadir una fila a la tabla
+    let nuevoTR = cuerpoTabla.insertRow()
+    let celda1 = nuevoTR.insertCell()
+    let celda2 = nuevoTR.insertCell()
+    let celda3 = nuevoTR.insertCell()
+    let celda4 = nuevoTR.insertCell()
+    celda1.textContent = studentName.value
+    celda2.textContent = choice1.selectedOptions[0].textContent
+    celda3.textContent = choice2.selectedOptions[0].textContent
+
+    let botonBorrar = document.createElement("button")
+    botonBorrar.classList.add("btn","btn-danger")
+    botonBorrar.textContent = "X"
+    celda4.append(botonBorrar)
+    botonBorrar.addEventListener("click", function(){
+        //borrar TR del HTML
+        nuevoTR.remove()
+        //borrar también objeto del array
+        let pos = choices.findIndex( a => a.name == celda1.textContent &&
+                                          a.choice1 == celda2.textContent &&
+                                          a.choice2 == celda3.textContent )
+        if (pos != -1) {
+            choices.splice(pos,1)
+        }
+    })
+}
