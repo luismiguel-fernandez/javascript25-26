37503a100652d8dfb1e857e4d7dc8a7b9100232d
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-10-27 12:04:27 +0100

    ej11 en progreso

--- ej11_lista_videojuegos/script.js
+++ ej11_lista_videojuegos/script.js
@@ -5,53 +5,35 @@ const platform = document.querySelector("#platform")
 const sendBtn = document.querySelector("#sendBtn")
 const videogamesList = document.querySelector("#videogamesList")
 
-//modelo = datos
 //let db = JSON.parse( localStorage.getItem("db") || "[]" )
 
-let db = [
-    { title: "The Legend of Zelda: Breath of the Wild", year: 2017, platform: "Nintendo Switch" },
-    { title: "God of War", year: 2018, platform: "PlayStation 4" },
-    { title: "Red Dead Redemption 2", year: 2018, platform: "PlayStation 4" },
-    { title: "Minecraft", year: 2011, platform: "Multiple" },
-    { title: "The Witcher 3: Wild Hunt", year: 2015, platform: "Multiple" },
-    { title: "Halo: Combat Evolved", year: 2001, platform: "Xbox" },
-    { title: "Super Mario Odyssey", year: 2017, platform: "Nintendo Switch" },
-    { title: "Cyberpunk 2077", year: 2020, platform: "Multiple" },
-    { title: "Dark Souls III", year: 2016, platform: "Multiple" },
-    { title: "Fortnite", year: 2017, platform: "Multiple" },
-    { title: "Call of Duty: Modern Warfare", year: 2019, platform: "Multiple" },
-    { title: "Overwatch", year: 2016, platform: "Multiple" },
-    { title: "Animal Crossing: New Horizons", year: 2020, platform: "Nintendo Switch" },
-    { title: "Sekiro: Shadows Die Twice", year: 2019, platform: "Multiple" },
-    { title: "Apex Legends", year: 2019, platform: "Multiple" },
-    { title: "Doom Eternal", year: 2020, platform: "Multiple" },
-    { title: "Among Us", year: 2018, platform: "Multiple" },
-    { title: "Final Fantasy VII Remake", year: 2020, platform: "PlayStation 4" },
-    { title: "Monster Hunter: World", year: 2018, platform: "Multiple" },
-    { title: "GTA V", year: 2013, platform: "Multiple" },
-    { title: "Destiny 2", year: 2017, platform: "Multiple" },
-    { title: "Minecraft Dungeons", year: 2020, platform: "Multiple" },
-    { title: "Super Smash Bros. Ultimate", year: 2018, platform: "Nintendo Switch" },
-    { title: "Metal Gear Solid V: The Phantom Pain", year: 2015, platform: "Multiple" },
-    { title: "Persona 5", year: 2016, platform: "PlayStation 3/4" },
-    { title: "Resident Evil 2 (Remake)", year: 2019, platform: "Multiple" },
-    { title: "Halo Infinite", year: 2021, platform: "Xbox Series X|S" },
-    { title: "Control", year: 2019, platform: "Multiple" },
-    { title: "Bloodborne", year: 2015, platform: "PlayStation 4" },
-    { title: "Hades", year: 2020, platform: "Multiple" }
-  ]
-  
+const db = [
+    {title: "hola", year: "1970", platform: "dos" },
+    {title: "adios", year: "1975", platform: "win" },
+    {title: "que tal", year: "1965", platform: "atari" }
+]
+arrayToTable(db,videogamesList)
 
-//refrescar la vista con los datos del modelo
-//arrayToTable(db,videogamesList)
+//encabezado TITULO del juego, clicable para ordenar
+const titleHead = document.querySelector("#videogamesList th")
+//const yearHead = document.querySelector("#videogamesList th:nth-child(2)")
+const yearHead = document.querySelectorAll("#videogamesList th")[1]
+//const platformHead = document.querySelector("#videogamesList th:nth-child(3)")
+const platformHead = document.querySelectorAll("#videogamesList th")[2]
+
+titleHead.addEventListener("click",function(){
+    //ordenar alfabéticamente por nombre
+    db.sort( (game1,game2) => {
+        if (game1.title.toLowerCase() < game2.title.toLowerCase()) return -1
+        else return 1
+    } )
+    //renderizar de nuevo el array ya ordenado
+    arrayToTable(db,videogamesList)
+})
 
 //db_filtered = db.filter( game => game.title.toLowerCase().includes("w") )
 
-//ordenar alfabéticamente por nombre
-db.sort( (game1,game2) => {
-    if (game1.title.toLowerCase() < game2.title.toLowerCase()) return -1
-    else return 1
-} )
+
 
 //ordenar numéricamente por año
 db.sort( (game1,game2) => {
@@ -60,8 +42,6 @@ db.sort( (game1,game2) => {
     //alternativa más compacta: restar equivale al if..else anterior (devuelve neg o pos)
     return game1.year - game2.year
 } )
-
-arrayToTable(db,videogamesList)
 
 function arrayToTable(array,table) {
     //mostrar el array indicado en la estructura table proporcionada
