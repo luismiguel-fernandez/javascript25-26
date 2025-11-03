b15e139477c10d5d5086244d5476af4de9502a34
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-11-03 11:41:26 +0100

    ej12 funcionando

--- ej12_chat/script.js
+++ ej12_chat/script.js
@@ -19,11 +19,29 @@ teclado.addEventListener("keyup",function(ev){
     }
 })
 
-let consultador = setInterval(consultarMensajes, 1000)
+let consultador = setInterval(consultarMensajes, 2000)
+let ultimoId = 0
 
 function consultarMensajes() {
-    fetch("server/chat_select_get_json.php?ultimo=0")
-    .post( data => {
+    fetch("server/chat_select_get_json.php?ultimo="+ultimoId)
         //procesar datos JSON y mostrar en el div CHAT todos los mensajes llegados en el JSON
+        //1.parsear la respuesta JSON que hemos recibido
+    .then( data => data.json() )
+    .then( arrayMensajes => {
+        //2. mostrar en pantalla el array resultante del parseado del JSON
+        console.log(arrayMensajes)
+        arrayMensajes.forEach( m => {
+            //imprimir el mensaje "m"
+            //un mensaje "m" tiene este aspecto:
+            //{"id":1,"nick":"luismi","texto":"hola","fecha":"2025-01-30 18:39:40"}
+            let nuevoDiv = document.createElement("DIV")
+            nuevoDiv.innerHTML = `
+                <h5>${m.nick}</h5>
+                <span>${m.texto}</span>
+            `
+            chat.append(nuevoDiv)
+            nuevoDiv.classList.add("mensaje")
+            ultimoId = m.id
+        })
     })
 }
